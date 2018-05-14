0af01f9c6ea919bd633bd9ef7667e116fcec90c8
Guan-Rong Wu <Roger-Wu@users.noreply.github.com>
2018-05-14 16:42:10 +0800

    Update ReturnVsAssign.sol

--- contracts/ReturnVsAssign.sol
+++ contracts/ReturnVsAssign.sol
@@ -1,9 +1,12 @@
+pragma solidity ^0.4.23;
+// #optimize=true&version=soljson-v0.4.23+commit.124ca40d.js
+
 contract A {
     function f() public view returns(uint a) {
-        return 1; // 202 gas
+        return 1; // 184 gas
     }
 
     function g() public view returns(uint a) {
-        a = 1; // 224 gas
+        a = 1; // 206 gas
     }
 }
